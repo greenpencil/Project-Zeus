bf249da1a37ac65ae7938f9c0390a2ba6c437f21
Lewis Campbell <insidific@outlook.com>
2016-10-29 17:20:37 +0100

    Setup the Login

---
+++ Website/index.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'Models/Functions.php';
+//If the User hasn't already been set then one is created
+if(!isset($user)) {
+    $user = new User();
+}
+$view = new stdClass();
+$view->pageTitle = 'Homepage';
+require_once('Views/index.phtml');
