1ce5c1fc0c7299141e4cacdcb1c8e1159445ad96
Katie Paxton-Fear <K.Paxton-Fear@edu.salford.ac.uk>
2016-10-30 01:46:13 +0100

    Database changes

--- zeus-1.sql
+++ zeus-1.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50539
 File Encoding         : 65001
 
-Date: 2016-10-30 00:31:06
+Date: 2016-10-30 01:45:55
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,15 +20,11 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `blocks`;
 CREATE TABLE `blocks` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `start` datetime DEFAULT NULL,
   `length` double DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- ----------------------------
--- Records of blocks
--- ----------------------------
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Table structure for blocks_channels
@@ -44,91 +40,68 @@ CREATE TABLE `blocks_channels` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- ----------------------------
--- Records of blocks_channels
--- ----------------------------
-
--- ----------------------------
 -- Table structure for channels
 -- ----------------------------
 DROP TABLE IF EXISTS `channels`;
 CREATE TABLE `channels` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `code` varchar(255) DEFAULT NULL,
   `number` int(11) DEFAULT NULL,
   `title` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- ----------------------------
--- Records of channels
--- ----------------------------
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Table structure for groups
 -- ----------------------------
 DROP TABLE IF EXISTS `groups`;
 CREATE TABLE `groups` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- ----------------------------
--- Records of groups
--- ----------------------------
 
 -- ----------------------------
 -- Table structure for programs
 -- ----------------------------
 DROP TABLE IF EXISTS `programs`;
 CREATE TABLE `programs` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   `channel_id` int(11) DEFAULT NULL,
   `image` varchar(255) DEFAULT NULL,
+  `short_desc` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `programs_belong_to_channels` (`channel_id`),
   CONSTRAINT `programs_belong_to_channels` FOREIGN KEY (`channel_id`) REFERENCES `channels` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- ----------------------------
--- Records of programs
--- ----------------------------
-
--- ----------------------------
 -- Table structure for users
 -- ----------------------------
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- ----------------------------
--- Records of users
--- ----------------------------
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Table structure for votes
 -- ----------------------------
 DROP TABLE IF EXISTS `votes`;
 CREATE TABLE `votes` (
-  `user_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `channel_id` int(11) NOT NULL,
   `block_id` int(11) NOT NULL,
   `timestamp` datetime NOT NULL,
   KEY `users_have_votes` (`user_id`),
   KEY `blocks_have_votes` (`block_id`),
   KEY `channels_have_votes` (`channel_id`),
-  CONSTRAINT `channels_have_votes` FOREIGN KEY (`channel_id`) REFERENCES `channels` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `blocks_have_votes` FOREIGN KEY (`block_id`) REFERENCES `blocks` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `channels_have_votes` FOREIGN KEY (`channel_id`) REFERENCES `channels` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `users_have_votes` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- ----------------------------
--- Records of votes
--- ----------------------------
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
